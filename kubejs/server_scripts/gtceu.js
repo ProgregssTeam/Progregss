737df23e0ced83d62bdfadde1f31f7ea2af47d72
CPearl0 <1269596929@qq.com>
2023-12-17 22:36:23 +0800

    add something

--- kubejs/server_scripts/gtceu.js
+++ kubejs/server_scripts/gtceu.js
@@ -1,5 +1,7 @@
 ServerEvents.recipes(event => {
     event.replaceInput({ mod: 'gtceu' }, '#forge:ingots/osmium', 'gtceu:osmium_ingot')
+    event.replaceInput({ mod: 'gtceu' }, '#forge:storage_blocks/osmium', 'gtceu:osmium_block')
+    event.replaceInput({ mod: 'gtceu' }, '#forge:nuggets/osmium', 'gtceu:osmium_nugget')
     event.replaceInput({ mod: 'gtceu' }, '#forge:dusts/osmium', 'gtceu:osmium_dust')
     event.remove({ id: 'gtceu:shaped/tiny_dust_disassembling_osmium' })
     event.remove({ id: 'gtceu:shaped/small_dust_disassembling_osmium' })
