8694b2af7bab66a5f002acc7aab7fc44d76bc0dd
CPearl0 <1269596929@qq.com>
2024-01-03 23:31:29 +0800

    add earth

---
+++ kubejs/startup_scripts/gtceu_multiblocks.js
@@ -0,0 +1,31 @@
+GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
+    event.create('basic_alternator')
+        .category('multiblock')
+        .setEUIO('out')
+        .setMaxIOSize(0, 0, 0, 0)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.ARC)
+        .setMaxTooltips(6);
+});
+
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('basic_alternator', 'multiblock')
+        .rotationState(RotationState.NON_Y_AXIS)
+        .recipeType('basic_alternator')
+        .pattern(definition => FactoryBlockPattern.start()
+            .aisle("CWC", "CWC", "#W#")
+            .aisle("CWC", "K#E", "CWC")
+            .aisle("CWC", "CWA", "#W#")
+            .where('A', Predicates.controller(Predicates.blocks(definition.get())))
+            .where('W', Predicates.blocks(GTBlocks.COIL_CUPRONICKEL.get()))
+            .where("C", Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get()))
+            .where('#', Predicates.any())
+            .where('K', Predicates.abilities(PartAbility.INPUT_KINETIC).setExactLimit(1))
+            .where('E', Predicates.abilities(PartAbility.OUTPUT_ENERGY).setExactLimit(1))
+            .build()
+        )
+        .workableCasingRenderer(
+            "gtceu:block/casings/solid/machine_casing_solid_steel",
+            "gtceu:block/multiblock/implosion_compressor", false
+        );
+});
